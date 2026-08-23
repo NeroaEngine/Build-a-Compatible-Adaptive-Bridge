71fc8e7f6bcd54f4a6c3898087f4a3e62a5afeb9
Tsfarrell38 <admin@vaycora.com>
2026-08-23 18:12:43 -0400

    Export Servo GPU frame source seam

--- src/servo_runtime/mod.rs
+++ src/servo_runtime/mod.rs
@@ -1,8 +1,10 @@
 mod command;
+mod frame;
 mod host;
 mod proxy;
 mod wake;
 
+pub use frame::{NoSharedGpuFrameSource, ServoFrameSource};
 pub use host::ServoHost;
 pub use proxy::ServoEngineProxy;
 pub use wake::{NoopServoHostNotifier, ServoHostNotifier};
