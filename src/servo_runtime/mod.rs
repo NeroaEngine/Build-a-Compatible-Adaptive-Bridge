9aa90f5e4feaf11d9725670f928d1c2f5f91d622
Tsfarrell38 <admin@vaycora.com>
2026-08-22 23:50:57 -0400

    Export Servo host runtime surface

--- src/servo_runtime/mod.rs
+++ src/servo_runtime/mod.rs
@@ -1,6 +1,10 @@
 mod command;
+mod host;
 mod proxy;
+mod wake;
 
+pub use host::ServoHost;
 pub use proxy::ServoEngineProxy;
+pub use wake::{NoopServoHostNotifier, ServoHostNotifier};
 
 pub(crate) use command::ServoCommand;
