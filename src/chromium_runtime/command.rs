aa47a86c53bfe9761eda32c701a90adaaf6b7dcc
Tsfarrell38 <admin@vaycora.com>
2026-08-23 18:55:49 -0400

    Add Chromium host command contract

---
+++ src/chromium_runtime/command.rs
@@ -0,0 +1,84 @@
+use tokio::sync::oneshot;
+use url::Url;
+
+use crate::engine::EngineError;
+use crate::types::{
+    ActivityState, BrowserInput, PortableWebState, SharedGpuSurface, ViewConfig, ViewId, Viewport,
+};
+
+pub(crate) type Reply<T> = oneshot::Sender<Result<T, EngineError>>;
+
+/// Renderer-independent command boundary for the Chromium compatibility host.
+///
+/// No CEF browser, render handler, process handle, temporary accelerated-paint
+/// handle, or platform window object may cross this channel.
+pub(crate) enum ChromiumCommand {
+    CreateView {
+        config: ViewConfig,
+        reply: Reply<ViewId>,
+    },
+    DestroyView {
+        view_id: ViewId,
+        reply: Reply<()>,
+    },
+    Navigate {
+        view_id: ViewId,
+        url: Url,
+        reply: Reply<()>,
+    },
+    Resize {
+        view_id: ViewId,
+        viewport: Viewport,
+        reply: Reply<()>,
+    },
+    Input {
+        view_id: ViewId,
+        input: BrowserInput,
+        reply: Reply<()>,
+    },
+    SetActivity {
+        view_id: ViewId,
+        activity: ActivityState,
+        reply: Reply<()>,
+    },
+    ExportState {
+        view_id: ViewId,
+        reply: Reply<PortableWebState>,
+    },
+    ImportState {
+        view_id: ViewId,
+        state: PortableWebState,
+        reply: Reply<()>,
+    },
+    AcquireFrame {
+        view_id: ViewId,
+        reply: Reply<Option<SharedGpuSurface>>,
+    },
+}
+
+impl ChromiumCommand {
+    pub(crate) fn fail(self, message: impl Into<String>) {
+        let message = message.into();
+        let error = || EngineError::Internal(message.clone());
+
+        match self {
+            Self::CreateView { reply, .. } => {
+                let _ = reply.send(Err(error()));
+            }
+            Self::DestroyView { reply, .. }
+            | Self::Navigate { reply, .. }
+            | Self::Resize { reply, .. }
+            | Self::Input { reply, .. }
+            | Self::SetActivity { reply, .. }
+            | Self::ImportState { reply, .. } => {
+                let _ = reply.send(Err(error()));
+            }
+            Self::ExportState { reply, .. } => {
+                let _ = reply.send(Err(error()));
+            }
+            Self::AcquireFrame { reply, .. } => {
+                let _ = reply.send(Err(error()));
+            }
+        }
+    }
+}
