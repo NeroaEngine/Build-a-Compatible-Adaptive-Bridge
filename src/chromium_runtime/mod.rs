34b7cddd31c003e44bc37c33f98e7f447cb72b29
Tsfarrell38 <admin@vaycora.com>
2026-08-23 22:02:51 -0400

    Export Chromium host runtime surface

--- src/chromium_runtime/mod.rs
+++ src/chromium_runtime/mod.rs
@@ -1,8 +1,12 @@
 mod command;
 mod frame;
+mod host;
 mod proxy;
+mod wake;
 
 pub use frame::{
     ChromiumAcceleratedFrame, ChromiumGpuFrameImporter, NoChromiumGpuFrameImporter,
 };
+pub use host::{ChromiumBackend, ChromiumHost, NoChromiumBackend};
 pub use proxy::ChromiumEngineProxy;
+pub use wake::{ChromiumHostNotifier, NoopChromiumHostNotifier};
